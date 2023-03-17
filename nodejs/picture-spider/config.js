16829cf2e399d3892c896f4ed95cd8bd7f165d8b
jack3933 <3099097649@qq.com>
2023-03-17 10:03:49 +0800

    使用nodejs的cheerio模块爬取图片

---
+++ nodejs/picture-spider/config.js
@@ -0,0 +1,7 @@
+const url = 'https://www.dm530p.net/'
+const path = require('path');
+const imgDir = path.join(__dirname, 'img');
+module.exports = {
+    url: url,
+    imgDir: imgDir
+};
---
+++ nodejs/picture-spider/index.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const path = require('path');
+const config = require('./config');
+const analyze = require('./analyze');
+const fs = require('fs');
+
+function start() {
+    request(config.url, function(err, res, body) {
+        if(!err && res) {
+            console.log('start');
+            analyze.findImg(body, downLoad);
+        }
+    })
+}
+
+function downLoad(imgUrl, i) {
+    let ext = imgUrl.split('.').pop();
+    request(imgUrl).pipe(fs.createWriteStream(path.join(config.imgDir, i+'.'+ext), {encoding: 'utf8'}));
+    console.log(i);
+}
+
+start();
