58f8a1f837fe312663d7360c906c653bd13ba173
kzw3933 <3099097649@qq.com>
2024-03-01 12:30:00 +0800

    移除部分内容

--- html+css+js/ui-music-player/script.js
+++
@@ -1,6 +0,0 @@
-const mod = document.querySelector('#mode');
-
-mod.addEventListener('click', () => {
-    document.querySelector('.phone').classList.toggle('dark');
-    document.querySelector('.bi-brightness-high-fill').classList.toggle('bi-moon');
-}); 
